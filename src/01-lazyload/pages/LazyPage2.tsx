ca378359e596965c7a9725fc376b55863bcbec8c
luigitemu <luartemu@gmail.com>
2021-11-08 10:35:48 -0600

    Paginas lazy agregadas

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,9 @@
+
+
+export const LazyPage2 = () => {
+    return (
+        <>
+            <h2>Lazy Page 2 </h2>   
+        </>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,9 @@
+
+
+export const LazyPage3 = () => {
+    return (
+        <>
+            <h2>Lazy Page 2 </h2>   
+        </>
+    )
+}
