03fe92640c4cb2bedc0193887a389006c686df45
luigitemu <luartemu@gmail.com>
2021-11-08 16:37:30 -0600

    Lazy Load Nested

---
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,15 @@
+import { Navigation } from "../router/Navigation";
+
+
+export const LazyLayout = () => {
+    return (
+        <div >
+            <h1>Lazy layout -  Main page</h1>            
+
+            <Navigation/>
+            
+        </div>
+    )
+}
+
+export default LazyLayout;
